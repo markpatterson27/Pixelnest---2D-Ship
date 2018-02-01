1f701c055e31cefbeb864f19487bbd7a51bba3bb
PAT09168900 <PAT09168900@BR-S3-6-06>
2018-02-01 16:03:26 +0000

    Added parallax scrolling and enemy behaviour.

--- Assets/scripts/EnemyScript.cs
+++ Assets/scripts/EnemyScript.cs
@@ -7,26 +7,88 @@ using UnityEngine;
 /// </summary>
 public class EnemyScript : MonoBehaviour {
 
+    private bool hasSpawn;
+    private MoveScript moveScript;
     private WeaponScript[] weapons;
+    private Collider2D coliderCompontent;
+    private SpriteRenderer rendererComponent;
 
     private void Awake()
     {
         // Retrieve the weapon only once
         weapons = GetComponentsInChildren<WeaponScript>();
+
+        // Retrieve scripts to disable when not spawn
+        moveScript = GetComponent<MoveScript>();
+
+        coliderCompontent = GetComponent<Collider2D>();
+
+        rendererComponent = GetComponent<SpriteRenderer>();
     }
 
-	// Update is called once per frame
-	void Update () {
+    // 1 - Disable everything
+    private void Start()
+    {
+        hasSpawn = false;
 
+        // Disable everything
+        // -- collider
+        coliderCompontent.enabled = false;
+        // -- moving
+        moveScript.enabled = false;
+        // -- shooting
         foreach (WeaponScript weapon in weapons)
         {
-            
-            // Auto-fire
-            if (weapon != null && weapon.CanAttack)
+            weapon.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // 2 - Check if the enemy has spawned.
+        if (hasSpawn == false)
+        {
+            if (rendererComponent.IsVisibleFrom(Camera.main))
             {
-                weapon.Attack(true);
+                Spawn();
+            }
+        }
+        else
+        {
+            foreach (WeaponScript weapon in weapons)
+            {
+
+                // Auto-fire
+                if (weapon != null && weapon.CanAttack)
+                {
+                    weapon.Attack(true);
+                }
+            }
+
+            // 4 - Out of the camera ? Destroy the game object.
+            if (rendererComponent.IsVisibleFrom(Camera.main) == false)
+            {
+                Destroy(gameObject);
             }
         }
 		
 	}
+
+    // 3 - Activate itself.
+    private void Spawn()
+    {
+        hasSpawn = true;
+
+        // Enable everything
+        // -- collider
+        coliderCompontent.enabled = true;
+        // -- moving
+        moveScript.enabled = true;
+        // -- shooting
+        foreach (WeaponScript weapon in weapons)
+        {
+            weapon.enabled = true;
+        }
+    }
 }
