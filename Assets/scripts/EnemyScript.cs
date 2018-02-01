fa74df9f9d81a1db59a845e14b9ac6e36c0fedb6
PAT09168900 <PAT09168900@BR-S3-6-06>
2018-02-01 14:19:05 +0000

    Added enemy weapon(s)

---
+++ Assets/scripts/EnemyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enemy generic behavior
+/// </summary>
+public class EnemyScript : MonoBehaviour {
+
+    private WeaponScript[] weapons;
+
+    private void Awake()
+    {
+        // Retrieve the weapon only once
+        weapons = GetComponentsInChildren<WeaponScript>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        foreach (WeaponScript weapon in weapons)
+        {
+            
+            // Auto-fire
+            if (weapon != null && weapon.CanAttack)
+            {
+                weapon.Attack(true);
+            }
+        }
+		
+	}
+}
