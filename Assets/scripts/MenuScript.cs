915f5438e6f5d67bfff1e4d198f2390645c77905
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2019-05-24 07:08:18 +0100

    created menu

---
+++ Assets/scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // "Stage1" is the name of the first scene we created.
+        Application.LoadLevel("Stage1");
+    }
+}
