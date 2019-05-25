9b468a1373520eecdd29d069a51d61445b9d8417
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2019-05-25 13:47:22 +0100

    added credits panel to menu

--- Assets/scripts/MenuScript.cs
+++ Assets/scripts/MenuScript.cs
@@ -7,9 +7,26 @@ using UnityEngine;
 /// </summary>
 public class MenuScript : MonoBehaviour
 {
+    private GameObject logo;
+    private GameObject credits;
+
+    void Start()
+    {
+        logo = GameObject.Find("Logo");
+        credits = GameObject.Find("CreditsPanel");
+        credits.SetActive(false);
+    }
+
     public void StartGame()
     {
         // "Stage1" is the name of the first scene we created.
         Application.LoadLevel("Stage1");
+    }
+
+    public void ShowCredits()
+    {
+        // enable/disable logo/credits panel
+        logo.SetActive(!logo.activeSelf);
+        credits.SetActive(!credits.activeSelf);
     }
 }
